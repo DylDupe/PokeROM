f232c8aee8a5015570e51af541b3dd93f0cdaa52
ddup5819 <ddup5819@uni.sydney.edu.au>
2019-11-13 10:18:06 +1100

    Structure complete

--- pokemon.py
+++
@@ -1,6 +0,0 @@
-import subprocess, argparse
-#comment
-parser = argparse.ArgumentParser()
-parser.add_argument("blue", help="This is Pokemon Mystery Dungeon Blue")
-parser.parse_args()
-
---
+++ sample/pokemon.py
@@ -0,0 +1,10 @@
+import subprocess, argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--blue", help="Pokemon Mystery Dungeon: Blue Rescue Team")
+args.parse_args()
+
+if args.blue:
+    print("Playing Pokemon Mystery Dungeon: Blue Rescue Team")
+
